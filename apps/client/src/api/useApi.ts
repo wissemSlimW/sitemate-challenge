20a2a6b8bb36e192923d6870f89afbf79f6046bd
wissem slim <wissem.slim@omegup.tn>
2024-08-23 16:30:22 +0200

    removing dummy Data

--- apps/client/src/api/useApi.ts
+++ apps/client/src/api/useApi.ts
@@ -14,55 +14,16 @@ export const useApi = <T>({
   handleError: (err: any) => void;
 }) => {
   useEffect(() => {
-    setData([
-      {
-        id: "000000",
-        title: "azeraz",
-        description:
-          "qddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-      },
-      {
-        id: "000001",
-        title: "azeraz",
-        description:
-          "qsdfqsd qsdfqscv qsdfqsd qsdfqsdfq qsdfqsd qsdfqsdfq fqsd fqsdf qsd fsqdf qsdf",
-      },
-
-      {
-        id: "000002",
-        title: "azeraz",
-        description:
-          "qsdfqsd qsdfqscv qsdfqsd qsdfqsdfq qsdfqsd qsdfqsdfq fqsd fqsdf qsd fsqdf qsdf",
-      },
-      {
-        id: "000003",
-        title: "azeraz",
-        description:
-          "qsdfqsd qsdfqscv qsdfqsd qsdfqsdfq qsdfqsd qsdfqsdfq fqsd fqsdf qsd fsqdf qsdf",
-      },
-      {
-        id: "000004",
-        title: "azeraz",
-        description:
-          "qsdfqsd qsdfqscv qsdfqsd qsdfqsdfq qsdfqsd qsdfqsdfq fqsd fqsdf qsd fsqdf qsdf",
-      },
-      {
-        id: "000005",
-        title: "azeraz",
-        description:
-          "qsdfqsd qsdfqscv qsdfqsd qsdfqsdfq qsdfqsd qsdfqsdfq fqsd fqsdf qsd fsqdf qsdf",
-      },
-    ] as any);
-    // setReady(false);
-    // axios
-    //   .get(`${BASE_URL}/${endpoint}`)
-    //   .then((response: AxiosResponse) => {
-    //     setData(response?.data || []);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     handleError(err?.response);
-    //   })
-    //   .finally(() => setReady(true));
+    setReady(false);
+    axios
+      .get(`${BASE_URL}/${endpoint}`)
+      .then((response: AxiosResponse) => {
+        setData(response?.data || []);
+      })
+      .catch((err) => {
+        console.log(err);
+        handleError(err?.response);
+      })
+      .finally(() => setReady(true));
   }, []);
 };
